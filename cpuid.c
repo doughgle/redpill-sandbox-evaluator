3fd149bb83a5fc4438610b1986fb16a2f4d0054f
Douglas Hellinger <doug.hellinger@hotmail.com>
2017-11-10 23:55:43 +0800

    simplify cpuid test

--- cpuid.c
+++ cpuid.c
@@ -1,19 +1,17 @@
 #include <cpuid.h>
 #include <stdio.h>
 
-int main (void)
+int is_hypervisor_present()
 {
-  int a, b, c, d;
-  if (!__get_cpuid (0x1, &a, &b, &c, &d))
-  {
-    fprintf (stderr, "Warning: CPUID request 0x81234567 unsupported!\n");
-  }
-  printf ("EAX: %x\nEBX: %x\nECX: %x\nEDX: %x\n", a, b, c, d);
-
-  if  (c & 0x80000000) {
-    printf("hypervisor detected! Taking red pill..!\n");
+  unsigned int eax, ebx, ecx, edx;
+  __get_cpuid(0x1, &eax, &ebx, &ecx, &edx);
+  if  (ecx & 0x80000000) {
     return 1;
   }
-  system("ping 8.8.8.8 -c 1");
   return 0;
+}
+
+int main (void)
+{
+  return is_hypervisor_present();
 }
